0736d22647e4635e5de9fc8b8da7f89351b29e23
Tim Schwartz <tim@metaverse.systems>
2016-07-17 12:46:32 -0500

    Client barebones

---
+++ client/config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if libjson-c exists. */
+#define HAVE_LIBJSON_C 1
+
+/* Name of package */
+#define PACKAGE "clairvoyance-client"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "tim@metaverse.systems"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "clairvoyance-client"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "clairvoyance-client 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "clairvoyance-client"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
